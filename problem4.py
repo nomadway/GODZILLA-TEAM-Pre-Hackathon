21bd0de22f26517047948556d2887f20fcc88693
nomadway <ubolxyz@gmail.com>
2021-02-25 13:09:08 +0600

    pre-hack4

--- problem4.py
+++ problem4.py
@@ -4,10 +4,23 @@
 # Создайте input() который принимает от пользователя дату в формате: "2020-10-24 18:30" и возвращает dictionary разделённую по значениям даты:
 
 
-data = {'year':input(),
-'month':input(),
-'day':input(),
-'hour':input()}
+# data = {'year':input(),
+# 'month':input(),
+# 'day':input(),
+# 'hour':input()}
 
-print(data)
+# print(data)
+
+
+
+print("Napishite дату в формате: 2020-10-24 18:30")
+a = input()
+b = input()
+c = input()
+d = input()
+
+q = {'year':a, 'month':b, 'day':c, 'time':d}
+# for value in q.items():
+    
+print(q)
 
